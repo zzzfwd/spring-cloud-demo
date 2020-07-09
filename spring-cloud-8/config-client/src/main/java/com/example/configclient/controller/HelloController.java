9e30def3ddd7e98a6d900cda6c0883e1562ce742
liu.hongjin <liu.hongjin@trs.com.cn>
2020-07-09 18:22:35 +0800

    commit spring cloud 8

---
+++ spring-cloud-8/config-client/src/main/java/com/example/configclient/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.configclient.controller;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RefreshScope
+@RestController
+public class HelloController {
+
+    @Value("${demo.hello}")
+    private String hello;
+
+    @RequestMapping("/hello")
+    public String hello() {
+        return this.hello;
+    }
+
+}
