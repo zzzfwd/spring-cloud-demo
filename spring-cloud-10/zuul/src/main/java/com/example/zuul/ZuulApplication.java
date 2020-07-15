aac29d7fbefa2bc62a636dc07b06da979a60396f
liu.hongjin <liu.hongjin@trs.com.cn>
2020-07-15 14:10:33 +0800

    commit spring cloud 10

--- spring-cloud-10/zuul/src/main/java/com/example/zuul/ZuulApplication.java
+++ spring-cloud-10/zuul/src/main/java/com/example/zuul/ZuulApplication.java
@@ -2,8 +2,10 @@ package com.example.zuul;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
+@EnableDiscoveryClient
 @EnableZuulProxy
 @SpringBootApplication
 public class ZuulApplication {
