4f0737c73a9a07ce0478a3a4998b4229bc443ad3
liu.hongjin <liu.hongjin@trs.com.cn>
2020-07-17 14:11:24 +0800

    commit spring cloud 12

---
+++ spring-cloud-12/zipkin-server/src/main/java/com/example/zipkinserver/ZipkinServerApplication.java
@@ -0,0 +1,17 @@
+package com.example.zipkinserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import zipkin2.server.internal.EnableZipkinServer;
+
+@EnableZipkinServer
+@EnableDiscoveryClient
+@SpringBootApplication
+public class ZipkinServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZipkinServerApplication.class, args);
+    }
+
+}
