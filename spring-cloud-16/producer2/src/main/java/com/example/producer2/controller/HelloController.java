ddd6e784a749ab8a08f8a137977dbdb1e3d518f6
liu.hongjin <liu.hongjin@trs.com.cn>
2020-07-21 14:39:10 +0800

    commit spring cloud 16

---
+++ spring-cloud-16/producer2/src/main/java/com/example/producer2/controller/HelloController.java
@@ -0,0 +1,20 @@
+package com.example.producer2.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @RequestMapping("/hello")
+    public String hello(@RequestParam("name") String name) {
+        return "Hello " + name + ", this is producer2.";
+    }
+
+    @RequestMapping("/foo")
+    public String foo(String foo) {
+        return "Hello " + foo + "!!";
+    }
+
+}
